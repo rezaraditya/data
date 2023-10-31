fa8c2a64798ad789305f83a29c564681f838813a
rezaraditya <60649124+rezaraditya@users.noreply.github.com>
2023-10-31 14:46:35 +0700

    Add files via upload

---
+++ uts.py
@@ -0,0 +1,24 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('diabet.sav', 'rb'))
+
+st.title ('Info diabetes')
+
+Glucose = st.number_input('input glukosa ')
+BloodPressure = st.number_input('input glukosa ')
+SkinThickness = st.number_input('input skin ')	
+Insulin	= st.number_input('input insul ')
+BMI	= st.number_input('input glukosa ')
+DiabetesPedigreeFunction = st.number_input('input dpf ')
+Age	= st.number_input('input umur ')
+Intercept= st.number_input('input intersep ')
+
+predict = ''
+
+
+if st.button('cek') :
+    predict = model.predict(
+        [[Glucose,BloodPressure,SkinThickness,Insulin,BMI, DiabetesPedigreeFunction, Age, Intercept]]
+    )
+    st.write('Estimasi Harga mobil dalam ponds : ', predict)
